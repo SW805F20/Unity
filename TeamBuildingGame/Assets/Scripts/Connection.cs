54d1b6d9e9ae00fc8544bfe68ec23b7e57122303
Daniel Moesgaard <moesgaarda@gmail.com>
2020-02-21 10:08:33 +0100

    Added github assets
    Added github assets

---
+++ TeamBuildingGame/Assets/Scripts/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : MonoBehaviour
+{
+    public int p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject PlayingField = GameObject.Find("PlayingField");
+        PlayingField.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
