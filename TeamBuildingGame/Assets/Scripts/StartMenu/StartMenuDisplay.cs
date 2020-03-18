2e4b6fa62a2d2d87aa633fef684fa7af3a5e29c4
Andreas Stenshøj <and8700and@gmail.com>
2020-03-18 11:28:20 +0100

    start menu

---
+++ TeamBuildingGame/Assets/Scripts/StartMenu/StartMenuDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+using System;
+public class StartMenuDisplay : MonoBehaviour
+{
+    GameObject display;
+    TMP_InputField inputField;
+    GameObject playersConnected;
+    TMP_Text connectionText;
+    public void Awake()
+    {
+        display = GameObject.Find("Display");
+        inputField = display.GetComponentInChildren<TMP_InputField>();
+        playersConnected = GameObject.Find("PlayersConnected");
+        connectionText = playersConnected.GetComponentInChildren<TMP_Text>();
+        playersConnected.SetActive(false);
+
+    }
+    public void ConnectToHost()
+    {
+
+        if(inputField == null)
+        {
+            Debug.Log("Could not find component");
+        }
+
+        // The provided IP in the input field should be used to connect to the host
+        // After succesful connection we check if all players are connected so the game can begin.
+        // Otherwise we display the number of players that are connected as a waiting screen.
+
+        if(!String.IsNullOrEmpty(inputField.text))
+        {
+            inputField.gameObject.SetActive(false);
+            connectionText.text = "Connected to Host \"Host IP\" \n (1 of 4 players connected)";
+            playersConnected.SetActive(true);
+        }
+
+    }
+
+    public void CancelConnection()
+    {
+        // Should cancel the connection to the host
+        inputField.gameObject.SetActive(true);
+        playersConnected.SetActive(false);
+    }
+}
