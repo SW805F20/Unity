8f6720c14a9debe5ae4c122ef853dbccdf07fa01
Rasmus <rasmusedu@gmail.com>
2020-03-20 09:55:23 +0100

    changes

---
+++ TeamBuildingGame/Assets/DisableTracking.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DisableTracking : MonoBehaviour
+{
+    public FieldGenerator playingField;
+    private List<float> xAnchors = new List<float>();
+    private List<float> yAnchors = new List<float>();
+    private float xMax, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xAnchors.Add(playingField.anchor1.x);
+        xAnchors.Add(playingField.anchor2.x);
+        xAnchors.Add(playingField.anchor3.x);
+        xAnchors.Add(playingField.anchor4.x);
+
+        yAnchors.Add(playingField.anchor1.y);
+        yAnchors.Add(playingField.anchor2.y);
+        yAnchors.Add(playingField.anchor3.y);
+        yAnchors.Add(playingField.anchor4.y);
+        xMax = xAnchors.Max();
+        yMax = yAnchors.Max();
+        this.GetComponent<Camera>().orthographicSize = yMax/2f;
+        this.GetComponent<Camera>().aspect = xMax / yMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TeamBuildingGame/Assets/Scripts/FieldGenerator.cs
+++ TeamBuildingGame/Assets/Scripts/FieldGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class FieldGenerator : MonoBehaviour
