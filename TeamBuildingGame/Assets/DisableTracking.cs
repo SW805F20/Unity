7ad44347a9c12a2e592ab27f5b4e92ebde1f5b49
Rasmus <rasmusedu@gmail.com>
2020-03-20 13:07:29 +0100

    should update now

--- TeamBuildingGame/Assets/DisableTracking.cs
+++ TeamBuildingGame/Assets/DisableTracking.cs
@@ -2,34 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class DisableTracking : MonoBehaviour
 {
-    public FieldGenerator playingField;
-    private List<float> xAnchors = new List<float>();
-    private List<float> yAnchors = new List<float>();
+    private bool cameraReset = false;
     private float xMax, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        xAnchors.Add(playingField.anchor1.x);
-        xAnchors.Add(playingField.anchor2.x);
-        xAnchors.Add(playingField.anchor3.x);
-        xAnchors.Add(playingField.anchor4.x);
-
-        yAnchors.Add(playingField.anchor1.y);
-        yAnchors.Add(playingField.anchor2.y);
-        yAnchors.Add(playingField.anchor3.y);
-        yAnchors.Add(playingField.anchor4.y);
-        xMax = xAnchors.Max();
-        yMax = yAnchors.Max();
-        this.GetComponent<Camera>().orthographicSize = yMax/2f;
-        this.GetComponent<Camera>().aspect = xMax / yMax;
+        StartCoroutine(SwitchTo2D());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xMax = gameObject.GetComponentInParent<MoveCameraToMiddle>().xMax;
+        yMax = gameObject.GetComponentInParent<MoveCameraToMiddle>().yMax;
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = xMax / yMax;
+        if (cameraReset)
+        {
+            if (screenRatio >= targetRatio)
+            {
+                this.GetComponent<Camera>().orthographicSize = yMax / 2f;
+            }
+            else
+            {
+                float sizeDifference = targetRatio / screenRatio;
+                this.GetComponent<Camera>().orthographicSize = yMax / 2f * sizeDifference;
+            }
+        }
+    }
+
+    // Call via `StartCoroutine(SwitchTo2D())` from your code. Or, use
+    // `yield SwitchTo2D()` if calling from inside another coroutine.
+    IEnumerator SwitchTo2D()
+    {
+        // Empty string loads the "None" device.
+        XRSettings.LoadDeviceByName("");
+
+        // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+        yield return null;
+
+        ResetCameras();
+    }
+
+    void ResetCameras()
+    {
+        // Camera looping logic copied from GvrEditorEmulator.cs
+        for (int i = 0; i < Camera.allCameras.Length; i++)
+        {
+            Camera cam = Camera.allCameras[i];
+            if (cam.enabled && cam.stereoTargetEye != StereoTargetEyeMask.None)
+            {
+
+                // Reset local position.
+                // Only required if you change the camera's local position while in 2D mode.
+                cam.transform.localPosition = Vector3.zero;
+
+                // Reset local rotation.
+                // Only required if you change the camera's local rotation while in 2D mode.
+                cam.transform.localRotation = Quaternion.identity;
+            }
+        }
+
+        cameraReset = true;
     }
 }
---
+++ TeamBuildingGame/Assets/MoveCameraToMiddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MoveCameraToMiddle : MonoBehaviour
+{
+    public FieldGenerator playingField;
+    private List<float> xAnchors = new List<float>();
+    private List<float> yAnchors = new List<float>();
+    public float xMax, yMax;
+    private float xMiddle, yMiddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xAnchors.Add(playingField.anchor1.x);
+        xAnchors.Add(playingField.anchor2.x);
+        xAnchors.Add(playingField.anchor3.x);
+        xAnchors.Add(playingField.anchor4.x);
+
+        yAnchors.Add(playingField.anchor1.y);
+        yAnchors.Add(playingField.anchor2.y);
+        yAnchors.Add(playingField.anchor3.y);
+        yAnchors.Add(playingField.anchor4.y);
+        xMax = xAnchors.Max();
+        yMax = yAnchors.Max();
+        xMiddle = xMax / 2f;
+        yMiddle = yMax / 2f;
+        transform.position = new Vector3(xMiddle, yMiddle, -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
