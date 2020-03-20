86571eac6ab5dcec50ae3c75d96cafe13ac96b18
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2020-03-20 14:31:27 +0100

    PROJ-29 : Added ObjectMover (#16)

---
+++ TeamBuildingGame/Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    GameObject client;
+    UDPClient clientData;
+
+    [SerializeField]
+    GameObject playerPrefab;
+    GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = GameObject.Find("CLIENT");
+        clientData = client.GetComponent<UDPClient>();
+
+        players = new GameObject[clientData.playerCount];
+        for (int i = 0; i < clientData.playerCount; i++)
+        {
+            players[i] = Instantiate(playerPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < clientData.playerCount; i++)
+        {
+            players[i].transform.position = clientData.playerPositions[i];
+        }
+    }
+}
