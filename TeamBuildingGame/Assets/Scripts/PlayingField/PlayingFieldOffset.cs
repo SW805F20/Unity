cf8865e6d7de557e37222d3f9c4ba0a344889754
Rasmus <rasmusedu@gmail.com>
2020-03-25 21:10:36 +0100

    it works now with meshes

--- TeamBuildingGame/Assets/Scripts/PlayingField/PlayingFieldOffset.cs
+++ TeamBuildingGame/Assets/Scripts/PlayingField/PlayingFieldOffset.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class MoveCameraToMiddle : MonoBehaviour
+public class PlayingFieldOffset : MonoBehaviour
 {
     public FieldGenerator playingField;
     private List<float> xAnchors = new List<float>();
     private List<float> yAnchors = new List<float>();
-    public float xMax, yMax;
-    public float xMiddle, yMiddle;
+    private float xMax, yMax, xMiddle, yMiddle;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Since the playingfield is a mesh, it is drawn from the bottom left, rather than the center as regular shapes.
+        // As such, an offset has to be introduced to ensure it is placed in the center of the camera.
+        // This is done by finding the maximum edge lengths, and then the midpoint such that it can be offset.
         xAnchors.Add(playingField.anchor1.x);
         xAnchors.Add(playingField.anchor2.x);
         xAnchors.Add(playingField.anchor3.x);
@@ -27,12 +28,13 @@ public class MoveCameraToMiddle : MonoBehaviour
         yMax = yAnchors.Max();
         xMiddle = xMax / 2f;
         yMiddle = yMax / 2f;
-        transform.position = new Vector3(xMiddle, yMiddle, -10f);
+
+        // Since meshes are drawn from the bottom left, the offset has to be the negative of the middle.
+        transform.position = new Vector3(-xMiddle, -yMiddle, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
