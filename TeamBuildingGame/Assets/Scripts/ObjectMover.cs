befdeb8e04493e76d1deea83717e7b8cc44b4186
Rasmus <rasmusedu@gmail.com>
2020-03-31 10:57:19 +0200

    added textures, fuck it looks so good

--- TeamBuildingGame/Assets/Scripts/ObjectMover.cs
+++ TeamBuildingGame/Assets/Scripts/ObjectMover.cs
@@ -11,6 +11,10 @@ public class ObjectMover : MonoBehaviour
     GameObject playerPrefab;
     GameObject[] players;
 
+    public Material blueColor;
+    public Material redColor;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,14 @@ public class ObjectMover : MonoBehaviour
         for (int i = 0; i < clientData.playerCount; i++)
         {
             players[i] = Instantiate(playerPrefab);
+            if (i < clientData.playerCount / 2)
+            {
+                players[i].GetComponent<MeshRenderer>().material = blueColor;
+            }
+            else
+            {
+                players[i].GetComponent<MeshRenderer>().material = redColor;
+            }
         }
     }
 
