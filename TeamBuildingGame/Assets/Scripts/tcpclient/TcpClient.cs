b9cd3e8b0d3e349b3021d56e195fe469e748aacc
Frederik <frederikhtxq12@gmail.com>
2020-04-16 11:50:16 +0200

    test fejl

--- TeamBuildingGame/Assets/Scripts/tcpclient/TcpClient.cs
+++ TeamBuildingGame/Assets/Scripts/tcpclient/TcpClient.cs
@@ -4,70 +4,73 @@ using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System;
-
-public class TcpClient : MonoBehaviour
+namespace sw8
 {
-    private Vector2[] goalPositions;
-    private GoalZoneController goalZoneController;
-
-    public GameObject playingfield;
-
-    // Start is called before the first frame update
-    void Start()
+    public class TcpClient : MonoBehaviour
     {
-        goalPositions = new Vector2[2];
-        goalZoneController = playingfield.GetComponent<GoalZoneController>();
-    }
+        private Vector2[] goalPositions;
+        private GoalZoneController goalZoneController;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public GameObject playingfield;
 
-    /// <summary> Receives the datagram. 
-    /// <param name="res">The result associated with the callback - the data.</param>
-    private void Receive(IAsyncResult res)
-    {
-
-    }
-
-    /// <summary> 
-    ///  Gets the datagram message and decodes it to find the type of message.
-    ///  Depending on the type it calls the appropiate method to handle the message.
-    /// </summary>
-    /// <param name="datagramMessage">The hex message send from the game server</param>
-    private void DatagramHandler(string datagramMessage)
-    {
-        // Remove 0x from string before parsing
-        datagramMessage = datagramMessage.Remove(0, 2);
-        long data;
-        byte type;
-        if (long.TryParse(datagramMessage, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out data))
+        // Start is called before the first frame update
+        void Start()
         {
-            type = (byte)data;
-            switch (type)
-            {
-                case 2:
-                    GoalPositionHandler(data);
-                    break;
+            goalPositions = new Vector2[2];
+            goalZoneController = playingfield.GetComponent<GoalZoneController>();
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        /// <summary> Receives the datagram. 
+        /// <param name="res">The result associated with the callback - the data.</param>
+        private void Receive(IAsyncResult res)
+        {
+
+        }
+
+        /// <summary> 
+        ///  Gets the datagram message and decodes it to find the type of message.
+        ///  Depending on the type it calls the appropiate method to handle the message.
+        /// </summary>
+        /// <param name="datagramMessage">The hex message send from the game server</param>
+        private void DatagramHandler(string datagramMessage)
+        {
+            // Remove 0x from string before parsing
+            datagramMessage = datagramMessage.Remove(0, 2);
+            long data;
+            byte type;
+            if (long.TryParse(datagramMessage, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out data))
+            {
+                type = (byte)data;
+                switch (type)
+                {
+                    case 2:
+                        GoalPositionHandler(data);
+                        break;
+
+                }
+            }
+            else
+            {
+                Debug.LogError("Network data could not be parsed");
             }
         }
-        else
+        /// <summary>
+        /// Updates the goals positions.
+        /// </summary>
+        /// <param name="data">The datagram message in hex with the 0x removed</param>
+        private void GoalPositionHandler(long data)
         {
-            Debug.LogError("Network data could not be parsed");
-        }
-    }
-    /// <summary>
-    /// Updates the goals positions.
-    /// </summary>
-    /// <param name="data">The datagram message in hex with the 0x removed</param>
-    private void GoalPositionHandler(long data)
-    {
-        // Get data correctly and save in goalPositions
+            // Get data correctly and save in goalPositions
 
-        goalZoneController.SpawnGoals(goalPositions[0], goalPositions[1]);
+            goalZoneController.SpawnGoals(goalPositions[0], goalPositions[1]);
+        }
+
     }
 
 }
