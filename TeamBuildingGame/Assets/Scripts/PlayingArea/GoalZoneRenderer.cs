326721ead5bd0d3b9cdf944b6ffa1e87cedf3a30
Frederik valdemar schrøder <frederikhtxq12@gmail.com>
2020-04-23 09:41:44 +0200

    Spawngoalsfix (#23)
    * Players should move when given input
    
    * unnity why
    
    * fixed ball
    
    * fixed pr request
    
    * deleted and refactored code
    
    * test fejl
    
    * fixed wrong order of anchor points
    
    * Update 72b85b-mainStage.json
    
    * Sync with develop
    
    * Update FieldGenerator.cs
    
    Co-authored-by: Daniel Moesgaard <moesgaarda@gmail.com>

--- TeamBuildingGame/Assets/Scripts/PlayingArea/GoalZoneRenderer.cs
+++ TeamBuildingGame/Assets/Scripts/PlayingArea/GoalZoneRenderer.cs
@@ -19,19 +19,16 @@ public class GoalZoneRenderer : MonoBehaviour
     }
 
     /// <summary>This method uses the anchor positions to draw the goalzone based on triangles.</summary>
-    /// <param name="anchor1">The bottom-left anchor for the goalzone.</param>
-    /// <param name="anchor2">The top-left anchor for the goalzone.</param>
-    /// <param name="anchor3">The bottom-right anchor for the goalzone.</param>
-    /// <param name="anchor4">The top-right anchor for the goalzone.</param>
-    public void MakeMeshData(Vector3 anchor1, Vector3 anchor2, Vector3 anchor3, Vector4 anchor4)
+    /// <param name="corners">All corners of the goal zone.</param>
+    public void MakeMeshData(Vector3[] corners)
     {
         mesh = GetComponent<MeshFilter>().mesh;
         // Define the four corners of the playing field, going in a clockwise direction.
-        vertices = new Vector3[] { anchor1, anchor2, anchor3, anchor4 };
+        vertices = corners;
         // Define how the triangles should be constructed for the mesh.
         // Should go from index 0 to 1 to 2 for first triangle and then
-        // 2, 1, 3 for the second to construct a square.
-        triangles = new int[] { 0, 1, 2, 2, 1, 3 };
+        // 2, 3, 0 for the second to construct a square.
+        triangles = new int[] { 0, 1, 2, 2, 3, 0};
 
         CreateMesh();
     }
