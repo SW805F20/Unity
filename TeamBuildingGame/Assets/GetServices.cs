a7304312a2275b7aee29a4ac1c7d1db85d9a556b
Rasmus <rasmusedu@gmail.com>
2020-04-27 14:29:39 +0200

    trying to add zeroconf

---
+++ TeamBuildingGame/Assets/GetServices.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zeroconf;
+
+public class GetServices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async Task EnumerateAllServicesFromAllHosts()
+    {
+        ILookup<string, string> domains = await ZeroconfResolver.BrowseDomainsAsync();
+        var responses = await ZeroconfResolver.ResolveAsync(domains.Select(g => g.Key));
+        foreach (var resp in responses)
+            Debug.Log(resp);
+    }
+}
