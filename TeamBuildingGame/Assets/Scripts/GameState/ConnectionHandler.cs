f757e2309f097b224f8db999c38955cba5bcd927
Daniel Moesgaard <moesgaarda@gmail.com>
2020-04-30 11:15:38 +0200

    Initial commit

---
+++ TeamBuildingGame/Assets/Scripts/GameState/ConnectionHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionHandler : MonoBehaviour
+{
+    public string tcpIPAddr;
+    public int tcpPort = 10000;
+}
--- TeamBuildingGame/Assets/Scripts/StartMenu/StartMenuDisplay.cs
+++ TeamBuildingGame/Assets/Scripts/StartMenu/StartMenuDisplay.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using System;
+using UnityEngine.SceneManagement;
+
 
 public class StartMenuDisplay : MonoBehaviour
 {
@@ -11,6 +13,14 @@ public class StartMenuDisplay : MonoBehaviour
     public InputField inputField;
     public GameObject playersConnected;
     public GameObject display;
+
+    [SerializeField]
+    private GameObject connectionHandlerObject;
+    ConnectionHandler connectionHandler;
+
+    public void Awake(){
+        connectionHandler = connectionHandlerObject.GetComponent<ConnectionHandler>();
+    }
 
     public void Start()
     {
@@ -31,11 +41,13 @@ public class StartMenuDisplay : MonoBehaviour
 
         if(!String.IsNullOrEmpty(inputField.text))
         {
+            connectionHandler.tcpIPAddr = inputField.text;
             inputField.gameObject.SetActive(false);
-            connectionText.text = $"Connected to host with IP {inputField.text} \n (1 of 4 players connected)";
+            connectionText.text = $"Connected to host with IP {connectionHandler.tcpIPAddr} \n (1 of 4 players connected)";
             playersConnected.SetActive(true);
         }
 
+        SceneManager.LoadScene("Main");
     }
 
     public void CancelConnection()
